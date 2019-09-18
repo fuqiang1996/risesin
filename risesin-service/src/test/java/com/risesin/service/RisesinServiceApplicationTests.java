e2926a70dbb466f3ec27aa7696c8f435a377d1ca
fuqiang1996 <315458488@qq.com>
2019-09-18 14:46:23 +0800

    修改单元父级测试类

--- risesin-service/src/test/java/com/risesin/service/RisesinServiceApplicationTests.java
+++ risesin-service/src/test/java/com/risesin/service/RisesinServiceApplicationTests.java
@@ -1,16 +1,25 @@
 package com.risesin.service;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+//测试web项目所加注解
+@WebAppConfiguration
 public class RisesinServiceApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    @Before
+    public void init() {
+        System.out.println("开始测试-----------------");
     }
 
+    @After
+    public void after() {
+        System.out.println("测试结束-----------------");
+    }
 }
