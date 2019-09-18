5bfe657a02eb8c494a908e9d241407b5a3a83fde
fuqiang1996 <315458488@qq.com>
2019-09-18 19:07:33 +0800

    测试类完善

--- risesin-service/src/test/java/com/risesin/service/commonuser/ChannelDefinitionServiceImplTest.java
+++ risesin-service/src/test/java/com/risesin/service/commonuser/ChannelDefinitionServiceImplTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -25,34 +26,43 @@ public class ChannelDefinitionServiceImplTest extends RisesinServiceApplicationT
     @Test
     public void findAll() {
         List<ChannelDefinition> all = channelDefinitionService.findAll();
-        Assert.assertTrue("查找所有信息",all.size() >=0 );
-    }
-
-    @Test
-    public void findSearch() {
+        Assert.assertTrue("查询信息为空",all.size() >=0 );
     }
 
     @Test
     public void findById() {
+        ChannelDefinition serviceById = channelDefinitionService.findById(3L);
+        Assert.assertTrue("根据id查询实体为空",serviceById.getId() == 3l);
     }
 
     @Test
     public void add() {
+        ChannelDefinition channelDefinition = new ChannelDefinition();
+        channelDefinition.setChaDefAddtime(new Date());
+        channelDefinition.setChaDefCode("123");
+        channelDefinition.setChaDefName("中汇");
+
+        channelDefinitionService.add(channelDefinition);
+        Assert.assertTrue("查询信息为空",channelDefinitionService.findAll().size() >=1 );
     }
 
     @Test
     public void update() {
+        ChannelDefinition channelDefinition = new ChannelDefinition();
+        channelDefinition.setChaDefAddtime(new Date());
+        channelDefinition.setChaDefCode("123");
+        channelDefinition.setChaDefName("短信");
+        channelDefinition.setId(1L);
+        channelDefinitionService.update(channelDefinition);
     }
 
-    @Test
-    public void testAdd() {
-    }
-
-    @Test
-    public void testUpdate() {
-    }
 
     @Test
     public void deleteById() {
+        channelDefinitionService.deleteById(3L);
+        List<ChannelDefinition> channelDefinitionList = channelDefinitionService.findAll();
+        channelDefinitionList.forEach((ChannelDefinition channelDefinition)->{
+            Assert.assertTrue("根据ID删除失败",channelDefinition.getId() == 3L);
+        });
     }
 }
--- risesin-service/src/test/java/com/risesin/service/commonuser/ComBranchCompanyServiceImplTest.java
+++ risesin-service/src/test/java/com/risesin/service/commonuser/ComBranchCompanyServiceImplTest.java
@@ -1,41 +1,80 @@
 package com.risesin.service.commonuser;
 
 import com.risesin.service.RisesinServiceApplicationTests;
+import com.risesin.service.modules.commonuser.serviceImpl.ComBranchCompanyServiceImpl;
+import com.risesin.service_api.modules.comuser.entity.ChannelDefinition;
+import com.risesin.service_api.modules.comuser.entity.ComBranchCompany;
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ComBranchCompanyServiceImplTest extends RisesinServiceApplicationTests {
 
-    @Test
-    public void findAll() {
-    }
+    @Autowired
+    private ComBranchCompanyServiceImpl comBranchCompanyService;
 
     @Test
-    public void findSearch() {
+    public void findAll() {
+        List<ComBranchCompany> all = comBranchCompanyService.findAll();
+        Assert.assertTrue("查询信息为空",all.size() >=0 );
     }
 
     @Test
     public void findById() {
+        ComBranchCompany serviceById = comBranchCompanyService.findById(1L);
+        Assert.assertTrue("根据id查询实体为空",serviceById.getId() == 1l);
     }
 
     @Test
     public void add() {
+        ComBranchCompany comBranchCompany = new ComBranchCompany();
+        comBranchCompany.setAddTime(new Date());
+        comBranchCompany.setBranArea("明城国际");
+        comBranchCompany.setBranBusinessAddress("湖南");
+        comBranchCompany.setBranName("com");
+        comBranchCompany.setBranRegAddress("中国");
+        comBranchCompany.setLastModified(new Date());
+        comBranchCompany.setLeader("leader");
+        comBranchCompany.setOrderNum(1L);
+        comBranchCompany.setLoanAgenId(1L);
+
+        comBranchCompanyService.add(comBranchCompany);
+        Assert.assertTrue("查询信息为空",comBranchCompanyService.findAll().size() >=1 );
     }
 
     @Test
     public void update() {
+        ComBranchCompany comBranchCompany = new ComBranchCompany();
+        comBranchCompany.setAddTime(new Date());
+        comBranchCompany.setBranArea("明城国际");
+        comBranchCompany.setBranBusinessAddress("湖南");
+        comBranchCompany.setBranName("com");
+        comBranchCompany.setBranRegAddress("中国");
+        comBranchCompany.setLastModified(new Date());
+        comBranchCompany.setLeader("aaaa");
+        comBranchCompany.setOrderNum(1L);
+        comBranchCompany.setLoanAgenId(1L);
+        comBranchCompany.setId(1l);
+
+        comBranchCompanyService.update(comBranchCompany);
+
+        ComBranchCompany byId = comBranchCompanyService.findById(1l);
+        Assert.assertTrue("根据id查询实体为空",byId.getLeader().equals("aaaa"));
+
     }
 
-    @Test
-    public void testAdd() {
-    }
-
-    @Test
-    public void testUpdate() {
-    }
 
     @Test
     public void deleteById() {
+        comBranchCompanyService.deleteById(1L);
+        List<ComBranchCompany> channelDefinitionList = comBranchCompanyService.findAll();
+        channelDefinitionList.forEach((ComBranchCompany comBranchCompany)->{
+            Assert.assertTrue("根据ID删除失败",comBranchCompany.getId() == 1L);
+        });
     }
 }
