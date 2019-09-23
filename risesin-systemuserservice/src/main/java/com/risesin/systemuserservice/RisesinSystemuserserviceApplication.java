88668ed9062288b7e863accd7cb4e2f0546822e1
fuqiang1996 <315458488@qq.com>
2019-09-23 15:01:34 +0800

    初始化系统服务项目

---
+++ risesin-systemuserservice/src/main/java/com/risesin/systemuserservice/RisesinSystemuserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.risesin.systemuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisesinSystemuserserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RisesinSystemuserserviceApplication.class, args);
+    }
+
+}
