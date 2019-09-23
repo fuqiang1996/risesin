f849170bb8fb201dfee8bd224ca8a35170b93044
fuqiang1996 <315458488@qq.com>
2019-09-23 14:58:49 +0800

    初始化代理项目

---
+++ risesin-agentuserservice/src/main/java/com/risesin/agentuserservice/RisesinAgentuserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.risesin.agentuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisesinAgentuserserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RisesinAgentuserserviceApplication.class, args);
+    }
+
+}
