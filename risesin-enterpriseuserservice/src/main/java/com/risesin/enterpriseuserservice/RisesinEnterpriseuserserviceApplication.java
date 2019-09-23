3815c223b9aa98d4e86a12c4ab8f758d9ab05641
fuqiang1996 <315458488@qq.com>
2019-09-23 14:54:54 +0800

    初始化 用户服务项目

---
+++ risesin-enterpriseuserservice/src/main/java/com/risesin/enterpriseuserservice/RisesinEnterpriseuserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.risesin.enterpriseuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisesinEnterpriseuserserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RisesinEnterpriseuserserviceApplication.class, args);
+    }
+
+}
