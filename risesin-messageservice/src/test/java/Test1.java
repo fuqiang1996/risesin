41b8dcf5a2b2d495756dc827fba082551371c3e1
Darling <751110514@qq.com>
2019-09-27 15:03:26 +0800

    提交测试类。

---
+++ risesin-messageservice/src/test/java/Test1.java
@@ -0,0 +1,20 @@
+import com.aliyuncs.exceptions.ClientException;
+import com.risesin.messageservice.core.SmsUtil;
+import org.junit.Test;
+
+/**
+ * @AUTHOR Darling
+ * @CREATE 2019-09-26
+ * @DESCRIPTION TODO
+ * @since 1.0.0
+ */
+
+//@ContextConfiguration(classes = MessageServiceApplicationTests.class)
+public class Test1 extends MessageServiceApplicationTests {
+
+
+    @Test
+    public void test() throws ClientException {
+
+    }
+}
