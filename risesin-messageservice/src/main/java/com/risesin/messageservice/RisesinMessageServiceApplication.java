0320d7afc0462a71d4c5fd79a7926ec136970279
Darling <751110514@qq.com>
2019-10-10 18:32:27 +0800

    各端服务注册到eureka。

--- risesin-messageservice/src/main/java/com/risesin/messageservice/RisesinMessageServiceApplication.java
+++ risesin-messageservice/src/main/java/com/risesin/messageservice/RisesinMessageServiceApplication.java
@@ -2,6 +2,7 @@ package com.risesin.messageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
 public class RisesinMessageServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RisesinMessageServiceApplication.class, args);
